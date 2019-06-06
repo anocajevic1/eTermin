9f2e3f45edc4fb9537cbe06c37d0bd7034c3229e
fpoljcic <fpoljcic1@etf.unsa.ba>
2019-06-06 15:05:28 +0200

    Sad valja

--- eTermin/Controllers/HomeController.cs
+++ eTermin/Controllers/HomeController.cs
@@ -20,5 +20,9 @@ namespace eTermin.Controllers {
         public IActionResult Error() {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult RecoveryForm1() {
+            return View();
+        }
     }
 }
